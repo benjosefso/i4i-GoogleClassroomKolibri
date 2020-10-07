44ae14f8a06e125630519f0ec181d67f91bfbda9
spazking55 <47401483+spazking55@users.noreply.github.com>
2020-10-07 19:19:32 -0400

    Added document retrieval and conversion
    requires install of slugify
    
    pip install slugify
    
    requires install of google drive api

--- gc_scraper.py
+++ gc_scraper.py
@@ -2,19 +2,27 @@ from __future__ import print_function
 import pickle
 import os.path
 import json
+import io
+from slugify import slugify
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from googleapiclient.http import MediaIoBaseDownload
+from googleapiclient.http import MediaFileUpload
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/classroom.courses.readonly',
 'https://www.googleapis.com/auth/classroom.coursework.students',
-'https://www.googleapis.com/auth/classroom.topics.readonly']
+'https://www.googleapis.com/auth/classroom.topics.readonly', 
+'https://www.googleapis.com/auth/drive.readonly',
+'https://www.googleapis.com/auth/drive.file']
 
 """
     Google Classroom Scraper by Ben Josefson 
     Script utilitzing Google Classroom API to scrape data from a Google Classroom Course
     
+    Also downloads and converts documents to pdf from Google Drive
+
     Access to a google account and permissions to access certain Google Classroom Data
     must be granted.
     
@@ -42,6 +50,7 @@ def main():
             pickle.dump(creds, token)
 
     service = build('classroom', 'v1', credentials=creds)
+    service2 = build('drive', 'v3', credentials=creds)
 
     #Create Comprehensive Course Class to be converted to json
     class CompleteCourse:
@@ -104,7 +113,149 @@ def main():
             else:     
                  for courseWork in courseworks:
                      #Store coursework data
-                     thisCourse.courseAssignments[courseWork['title']]=courseWork
+                     thisCourse.courseAssignments[courseWork["title"]]=courseWork
+
+                     #Check for DriveFiles to be downloaded
+                     if "materials" in thisCourse.courseAssignments[courseWork["title"]]:
+                        for spec_mat in thisCourse.courseAssignments[courseWork["title"]]["materials"]:
+                            if "driveFile" in spec_mat:
+                                file_name = slugify(spec_mat["driveFile"]["driveFile"]["title"])
+                                file_id = spec_mat["driveFile"]["driveFile"]["id"]
+
+                                file = service2.files().get(fileId=file_id).execute()
+                                print(file["mimeType"])
+
+                                #WORD DOCUMENT
+                                if file["mimeType"] == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+                                    request = service2.files().get_media(fileId=file_id)
+                                    fh = io.BytesIO()
+                                    downloader = MediaIoBaseDownload(fh, request)
+                                    done = False
+                                    while done is False:
+                                        status, done = downloader.next_chunk()
+
+                                    print(fh)
+                                    with open("tempdoc.docx",'wb') as out:
+                                        out.write(fh.getvalue())
+
+                                    file_metadata = {
+                                        'name': 'My Report',
+                                        'mimeType': 'application/vnd.google-apps.document'
+                                    }
+                                    media = MediaFileUpload('temploc.docx',resumable=True)
+                                    file = service2.files().create(body=file_metadata,media_body=media,fields='id').execute()
+                                    file_id = file['id']
+                                    converted = service2.files().export_media(fileId=file_id,mimeType='application/pdf')
+                                    fh = io.BytesIO()
+                                    downloader = MediaIoBaseDownload(fh, converted)
+                                    done = False
+                                    while done is False:
+                                        status, done = downloader.next_chunk()
+
+                                    with open(file_name + ".pdf",'wb') as out:
+                                        out.write(fh.getvalue())
+
+
+                                #GOOGLE DOCUMENT
+                                elif file["mimeType"] == "application/vnd.google-apps.document":
+                                    converted = service2.files().export_media(fileId=file_id,mimeType='application/pdf')
+                                    fh = io.BytesIO()
+                                    downloader = MediaIoBaseDownload(fh, converted)
+                                    done = False
+                                    while done is False:
+                                        status, done = downloader.next_chunk()
+
+                                    with open(file_name + ".pdf",'wb') as out:
+                                        out.write(fh.getvalue())
+
+
+                                #POWERPOINT PRESENTATION
+                                elif file["mimeType"] == "application/vnd.openxmlformats-officedocument.presentationml.presentation":
+                                    request = service2.files().get_media(fileId=file_id)
+                                    fh = io.BytesIO()
+                                    downloader = MediaIoBaseDownload(fh, request)
+                                    done = False
+                                    while done is False:
+                                        status, done = downloader.next_chunk()
+
+                                    print(fh)
+                                    with open("temploc.pptx",'wb') as out:
+                                        out.write(fh.getvalue())
+
+                                    file_metadata = {
+                                        'name': 'My Presentation',
+                                        'mimeType': 'application/vnd.google-apps.presentation'
+                                    }
+                                    media = MediaFileUpload('temploc.pptx',resumable=True)
+                                    file = service2.files().create(body=file_metadata,media_body=media,fields='id').execute()
+                                    file_id = file['id']
+                                    converted = service2.files().export_media(fileId=file_id,mimeType='application/pdf')
+                                    fh = io.BytesIO()
+                                    downloader = MediaIoBaseDownload(fh, converted)
+                                    done = False
+                                    while done is False:
+                                        status, done = downloader.next_chunk()
+
+                                    with open(file_name + ".pdf",'wb') as out:
+                                        out.write(fh.getvalue())
+
+
+                                #GOOGLE SLIDES
+                                elif file["mimeType"] == "application/vnd.google-apps.presentation":
+                                    converted = service2.files().export_media(fileId=file_id,mimeType='application/pdf')
+                                    fh = io.BytesIO()
+                                    downloader = MediaIoBaseDownload(fh, converted)
+                                    done = False
+                                    while done is False:
+                                        status, done = downloader.next_chunk()
+
+                                    with open(file_name + ".pdf",'wb') as out:
+                                        out.write(fh.getvalue())
+
+
+                                #EXCEL DOCUMENT
+                                elif file["mimeType"] == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+                                    request = service2.files().get_media(fileId=file_id)
+                                    fh = io.BytesIO()
+                                    downloader = MediaIoBaseDownload(fh, request)
+                                    done = False
+                                    while done is False:
+                                        status, done = downloader.next_chunk()
+
+                                    print(fh)
+                                    with open("temploc.xlsx",'wb') as out:
+                                        out.write(fh.getvalue())
+
+                                    file_metadata = {
+                                        'name': 'My Spreadsheet',
+                                        'mimeType': 'application/vnd.google-apps.spreadsheet'
+                                    }
+                                    media = MediaFileUpload('temploc.xlsx',resumable=True)
+                                    file = service2.files().create(body=file_metadata,media_body=media,fields='id').execute()
+                                    file_id = file['id']
+                                    converted = service2.files().export_media(fileId=file_id,mimeType='application/pdf')
+                                    fh = io.BytesIO()
+                                    downloader = MediaIoBaseDownload(fh, converted)
+                                    done = False
+                                    while done is False:
+                                        status, done = downloader.next_chunk()
+
+                                    with open(file_name + ".pdf",'wb') as out:
+                                        out.write(fh.getvalue())
+
+
+                                #GOOGLE SHEETS
+                                elif file["mimeType"] == "application/vnd.google-apps.spreadsheet":
+                                    converted = service2.files().export_media(fileId=file_id,mimeType='application/pdf')
+                                    fh = io.BytesIO()
+                                    downloader = MediaIoBaseDownload(fh, converted)
+                                    done = False
+                                    while done is False:
+                                        status, done = downloader.next_chunk()
+
+                                    with open(file_name + ".pdf",'wb') as out:
+                                        out.write(fh.getvalue())
+                                    
                      
         #Dump all course information to JSON file gc_data.json
         json.dump(thisCourse,gc_data,indent=1,default=encode_CompleteCourse)
